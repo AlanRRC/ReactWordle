e4823d8246b6fffd2fce2e2bfa1a4c7a07f4714a
Alan Simpson <asimpson@rrc.ca>
2022-05-03 14:45:55 -0500

    Showing the current guess

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -5,6 +5,9 @@ export default function Grid({ currentGuess, guesses, turn }) {
     return (
         <div>
             {guesses.map((guess, i) => {
+                if (turn === i) {
+                    return <Row key={i} currentGuess={currentGuess} />
+                }
                 return <Row key={i} guess={guess} />
             })}
         </div>
