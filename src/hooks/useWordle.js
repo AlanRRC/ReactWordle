229afae1b073d5c8a6de966dcac0e5173f0d36f1
Alan Simpson <asimpson@rrc.ca>
2022-05-09 09:43:05 -0500

    Update keyboard color changes

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
     const [guesses, setGuesses] = useState([...Array(6)]) // each guess is an array
     const [history, setHistory] = useState([]) // each guess is a string
     const [isCorrect, setIsCorrect] = useState(false)
+    const [usedKeys, setUsedKeys] = useState({}) // {a: 'green', b: 'yellow', c: 'grey'}
 
     //  format a new guess
     //  e.g. [{key: 'a', color:'yellow'}]
@@ -56,6 +57,29 @@ const useWordle = (solution) => {
             return prevTurn + 1
         })
 
+        setUsedKeys((prevUsedKeys) => {
+            let newKeys = { ...prevUsedKeys }
+
+            formattedGuess.forEach((l) => {
+                const currentColor = newKeys[l.key]
+
+                if (l.color === 'green') {
+                    newKeys[l.key] = 'green'
+                    return
+                }
+                if (l.color === 'yellow' && currentColor !== 'green') {
+                    newKeys[l.key] = 'yellow'
+                    return
+                }
+                if (l.color === 'grey' && currentColor !== 'green' && currentColor !== 'yellow') {
+                    newKeys[l.key] = 'grey'
+                    return
+                }
+            })
+
+            return newKeys
+        })
+
         setCurrentGuess("")
     }
 
@@ -101,7 +125,7 @@ const useWordle = (solution) => {
         }
     }
 
-    return { turn, currentGuess, guesses, isCorrect, handleKeyup }
+    return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup }
 }
 
 export default useWordle
